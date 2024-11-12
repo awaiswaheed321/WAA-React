53ff34574149988d76dd526a33778970f58292e4
Awais Waheed <awaiswaheed321@gmail.com>
2024-11-12 12:00:26 -0600

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import './App.css'
 import Dashboard from './components/Dashboard'
 
 function App() {
-
     return (
-        <>
-            <Dashboard/>
-        </>
+        <Dashboard/>
     )
 }
 
