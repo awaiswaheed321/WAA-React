976c7495a1441761a74596b8db69797e02c70d71
Awais Waheed <awaiswaheed321@gmail.com>
2024-11-15 14:59:36 -0600

    useReducer example

--- src/ReducerComponent.jsx
+++ src/ReducerComponent.jsx
@@ -26,13 +26,6 @@ const ReducerComponent = () => {
                 return state;
             }
         }
-        if (action.type === "increment") {
-            return {count: state.count + 1};
-        } else if (action.type === "decrement") {
-            return {count: state.count - 1};
-        } else if (action.type === "reset") {
-            return {count: 0};
-        }
     }
 
     const [state, dispatch] = useReducer(reducer, initialState);
