904140de414cb23d9db951676c8f454e012717da
Awais Waheed <awaiswaheed321@gmail.com>
2024-11-15 22:40:14 -0600

    redux example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-import Clock from './Clock'
-function App() {
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, incrementByAmount, reset} from "./state/counter.js";
 
-  return (
-    <>
-      <Clock></Clock>
-    </>
-  )
+function App() {
+    const {count} = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+    return (<div>
+        <h1>The count is: {count}</h1>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>Increment by 10</button>
+    </div>)
 }
 
 export default App
